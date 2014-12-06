b783ed4329166c458b58c0f8c246d315b53df58e
hassansin <mhkr63@gmail.com>
2014-12-06 21:42:56 +0600

    removed account creds

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 	'use strict';
 	exports.users = [
 		{
-			username: 'ppshirleydu@gmail.com',
-			password: 'shirleydu1'
+			username: '',
+			password: ''
 		},
 		{
-			username: 'eattacoswithdad@gmail.com',
-			password: 'happymeal2'
+			username: '',
+			password: ''
 		}
 	];
 })();
